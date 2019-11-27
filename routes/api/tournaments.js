b9a078bd9461b1cbf4475d4630377af9a1e6dc0a
Dustin Cintron <D.Cint88@gmail.com>
2019-11-27 01:06:12 -0500

    Created tournament POST route for creating new tournament items

--- routes/api/tournaments.js
+++ routes/api/tournaments.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Tournament model
 const Tournament = require('../../models/Tournament');
 
+
 // @route		GET api/tournaments
 // @descrip	Get All, INDEX
 // @access	Public
@@ -12,6 +13,21 @@ router.get('/', (req, res) => {
 		.sort({ date: -1 })															// -1 is descending order; 1 is ascending order
 		.then(tournaments => res.json(tournaments))
 		.catch(err => console.log(err));
+});
+
+
+// @route 	POST api/tournaments
+// @descrip NEW/CREATE
+// @access 	Public (for now.  Authentication later)
+router.post('/new', (req, res) => {
+	// const { title, description } = req.body
+	const newTournament = new Tournament({
+		title: req.body.title,
+		description: req.body.description
+	});
+
+	newTournament.save()
+		.then(tournament => res.json(tournament))
 });
 
 module.exports = router;
