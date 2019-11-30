2ba0702e4384b50d3d259828f6f04e8826b436c7
Dustin Cintron <D.Cint88@gmail.com>
2019-11-30 18:10:47 -0500

    Set up Modal components for user auth in it's own folder; Wrote SwitchRoutes in App.js for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavigationBar from './components/layout/NavigationBar';
 import ProfileSidebar from './components/layout/ProfileSidebar';
+import TournamentIndex from './components/tournaments/Index';
+import UserProfile from './components/users/UserProfile';
+import UserTournaments from './components/users/UserTournaments';
+import SmashNews from './components/news/SmashNews';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
+  	<Router>
+  		<NavigationBar />
       <ProfileSidebar />
-    </div>
+      <Switch>
+      	<Route exact path="/" component={TournamentIndex} />
+      	<Route path="/profile" component={UserProfile} />
+      	<Route path="/my-tournaments" component={UserTournaments} />
+      	<Route path="/smash-news" component={SmashNews} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/news/SmashNews.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class SmashNews extends Component {
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>smash bros.com</li>
+					<li>dlc news</li>
+					<li>latest patch notes</li>
+					<li>some site for latest professional tournaments (or pro youtube page)</li>
+				</ul>
+			</div>
+		)
+	}
+};
