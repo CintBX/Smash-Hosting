3e8016b24716205d10138ac5ff751c416fd1caba
Dustin Cintron <D.Cint88@gmail.com>
2019-12-05 11:41:20 -0500

    Wrapped the entire application within the react-redux Provider, making the store(and your State) available to the whole app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import NavigationBar from './components/layout/NavigationBar';
 import ProfileSidebar from './components/layout/ProfileSidebar';
 import TournamentIndex from './components/tournaments/Index';
@@ -17,28 +20,30 @@ import EditTournament from './components/tournaments/Edit';
 
 function App() {
   return (
-  	<Router>
-  		<NavigationBar />
-      <Container>
-        <Row>
-          <Col lg="4">
-            <ProfileSidebar />
-          </Col>
-        
-          <Col lg="8">
-            <Switch>
-            	<Route exact path="/" component={TournamentIndex} />
-            	<Route path="/profile" component={UserProfile} />
-            	<Route path="/smash-news" component={SmashNews} />
-              <Route path="/roster" component={FighterDirectory} />
-              <Route path="/tournament/:id" component={TournamentShow} />
-              <Route path="/tournament-new" component={NewTournament} />
-              <Route path="/tournament-edit" component={EditTournament} />
-            </Switch>
-          </Col>
-        </Row>
-      </Container>
-    </Router>
+    <Provider store={store}>
+    	<Router>
+    		<NavigationBar />
+        <Container>
+          <Row>
+            <Col lg="4">
+              <ProfileSidebar />
+            </Col>
+          
+            <Col lg="8">
+              <Switch>
+              	<Route exact path="/" component={TournamentIndex} />
+              	<Route path="/profile" component={UserProfile} />
+              	<Route path="/smash-news" component={SmashNews} />
+                <Route path="/roster" component={FighterDirectory} />
+                <Route path="/tournament/:id" component={TournamentShow} />
+                <Route path="/tournament-new" component={NewTournament} />
+                <Route path="/tournament-edit" component={EditTournament} />
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
+    </Provider>
   );
 }
 
