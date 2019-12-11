1e471fb262aa537840bc02f9cca2dacb097fbb70
Dustin Cintron <D.Cint88@gmail.com>
2019-12-11 11:25:05 -0500

    Implemented bCryptJS for password hashes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // User Model
 const User = require('../../models/User');
@@ -13,7 +14,7 @@ router.post('/new', (req, res) => {
 
 	// Validation (DO THIS FOR TOURNAMENTS LATER)
 	if (!username || !password) {
-		return res.status(400).json({ msg: "Username and Email are required" });
+		return res.status(400).json({ msg: "Username and Password are required" });
 	};
 
 	// Check for existing Username
@@ -21,6 +22,22 @@ router.post('/new', (req, res) => {
 		if(userFound) return res.status(400).json({ msg: "User already exists" });
 
 		const newUser = new User({ username, password });
+
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(newUser.password, salt, (err, hash) => {
+				if(err) throw err;
+				newUser.password = hash;
+				newUser.save()
+					.then(user => {
+						res.json({
+							user: {
+								id: user.id,
+								username: user.username
+							}
+						})
+					});
+			});
+		});
 	});
 });
 
