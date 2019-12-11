9579fba951dac6a8828363eefb0505273d38fb7b
Dustin Cintron <D.Cint88@gmail.com>
2019-12-11 12:18:31 -0500

    Finished setting up User registration with JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
 
 // User Model
 const User = require('../../models/User');
@@ -29,12 +32,21 @@ router.post('/new', (req, res) => {
 				newUser.password = hash;
 				newUser.save()
 					.then(user => {
-						res.json({
-							user: {
-								id: user.id,
-								username: user.username
+						jwt.sign(
+							{ id: user.id },
+							config.get('jwtSecret'),
+							{ expiresIn: 3600 },
+							(err, token) => {
+								if(err) throw err;
+								res.json({
+									token,
+									user: {
+										id: user.id,
+										username: user.username
+									}
+								})
 							}
-						})
+						)
 					});
 			});
 		});
