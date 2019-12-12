f67e85d6d97e153409964e91bab227d84bfc685d
Dustin Cintron <D.Cint88@gmail.com>
2019-12-12 16:28:10 -0500

    Configured authActions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,45 @@
+import {
+	USER_LOADING,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT_SUCCESS,
+	REGISTER_SUCCESS,
+	REGISTER_FAIL
+} from './types';
+import axios from 'axios';
+import { returnErrors } from './errorActions';
+
+// Check token and load user
+export const loadUser = () => (dispatch, getState) => {
+	// User Loading
+	dispatch({ type: USER_LOADING });
+
+	// Get Token from LocalStorage
+	const token = getState().auth.token;
+
+	// Headers
+	const config = {
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+
+	// If there's a token, add it to Headers
+	if(token) {
+		config.headers['x-auth-token'] = token;
+	};
+
+	axios.get('/users/user', config)
+		.then(res => dispatch({
+			type: USER_LOADED,
+			payload: res.data
+		}))
+		.catch(err => {
+			dispatch(returnErrors(err.response.data, err.response.status));
+			dispatch({
+				type: AUTH_ERROR
+			});
+		});
+};
