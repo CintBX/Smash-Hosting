551923508348cffb077d71fabd92e026c1afcc25
Dustin Cintron <D.Cint88@gmail.com>
2019-12-12 16:27:52 -0500

    Refactored App.js from a functional component to a Class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -15,32 +15,37 @@ import UserProfile from './components/users/UserProfile';
 import SmashNews from './components/news/SmashNews';
 import FighterDirectory from './components/users/FighterDirectory';
 
+import { loadUser } from './actions/authActions';
 
-function App() {
-  return (
-    <Provider store={store}>
-    	<Router>
-    		<NavigationBar />
-        <Container>
-          <Row>
-            <Col lg="4">
-              <ProfileSidebar />
-            </Col>
-          
-            <Col lg="8">
-              <Switch>
-              	<Route exact path="/" component={TournamentIndex} />
-              	<Route path="/profile" component={UserProfile} />
-              	<Route path="/smash-news" component={SmashNews} />
-                <Route path="/roster" component={FighterDirectory} />
-                <Route path="/tournament/:id" component={TournamentShow} />
-              </Switch>
-            </Col>
-          </Row>
-        </Container>
-      </Router>
-    </Provider>
-  );
-}
+export default class App extends Component{
+  componentDidMount() {
+    store.dispatch(loadUser());
+  };
 
-export default App;
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <NavigationBar />
+          <Container>
+            <Row>
+              <Col lg="4">
+                <ProfileSidebar />
+              </Col>
+            
+              <Col lg="8">
+                <Switch>
+                  <Route exact path="/" component={TournamentIndex} />
+                  <Route path="/profile" component={UserProfile} />
+                  <Route path="/smash-news" component={SmashNews} />
+                  <Route path="/roster" component={FighterDirectory} />
+                  <Route path="/tournament/:id" component={TournamentShow} />
+                </Switch>
+              </Col>
+            </Row>
+          </Container>
+        </Router>
+      </Provider>
+    );
+  };
+};
