1af5aabbb47712c8b3914a9e5e19a04d0df1c0ed
Dustin Cintron <D.Cint88@gmail.com>
2019-12-31 13:39:35 -0500

    Deleted 'description' from Tournament model, and added 'participants' array

--- models/Tournament.js
+++ models/Tournament.js
@@ -12,11 +12,6 @@ const TournamentSchema = new Schema({
 		required: false,
 		default: 'TestUser'
 	},
-	description: {
-		type: String,
-		required: false,
-		default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-	},
 	status: {
 		type: String,
 		required: false,
@@ -26,8 +21,11 @@ const TournamentSchema = new Schema({
 		type: Date,
 		default: Date.now
 	},
+	participants: {
+		type: Array,
+		required: false
+	}
 	// entranceFee: { tthis may be added later.  But start it off where this isn't a thing, just free tourneys for now }
-	// entrants: { tthis may be added later.  But start it off where this isn't a thing, just free tourneys for now }
 });
 
 module.exports = Tournament = mongoose.model('tournament', TournamentSchema);
