4d5af3a2dbdb92c19fe7695400134b9401ed1b89
Dustin <D.Cint88@gmail.com>
2020-04-14 13:03:18 -0400

    A user can sign up to a tournament

--- client/src/actions/participantActions.js
+++ client/src/actions/participantActions.js
@@ -3,11 +3,19 @@ import axios from 'axios';
 import { tokenConfig } from './authActions';
 import { returnErrors } from './errorActions';
 
-export const addParticipant = user => (dispatch, getState) => {
-  axios.post('/tournaments/:id', tokenConfig(getState))
+export const addParticipant = (_id, user) => dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const body = JSON.stringify({ user });
+  
+  axios.post(`/tournaments/${_id}`, body, config)
     .then(res => dispatch({
       type: USER_JOINS_TOURNAMENT,
-      payload: user
+      payload: res.data
     }))
     .catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
--- routes/api/tournaments.js
+++ routes/api/tournaments.js
@@ -67,14 +67,13 @@ module.exports = router;
 // @route		POST /tournaments/:id
 // @descrip	Add User to Tournament.participants array / User sign up
 // @access	Private(There must be a user to sign up)
-router.post('/:id', authorize, (req, res) => {
+// NOTE: `authorize` doesn't work for some reason.  May be ok, investigate later
+router.post('/:id', (req, res) => {
 	Tournament.findById(req.params.id)
 		.then(tournament => {
 			tournament.participants.push(req.body);
-			// save updated tournament in the DB, then promise-chain with another `then` before the Catch
 			return tournament.save();
 		})
-		// send a response to the client
 		.then(savedTournament => res.json(savedTournament))
 		.catch(err => res.json(err));
 });
