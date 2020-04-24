03bcae3cb039746e59d06ce4fd9b22447199678a
Dustin <D.Cint88@gmail.com>
2020-04-24 19:22:35 -0400

    Minor adjustments to button styling

--- client/src/components/tournaments/buttons.js
+++ client/src/components/tournaments/buttons.js
@@ -9,20 +9,19 @@ import {
 } from 'reactstrap';
 
 export const SignUp = (props) => {
-		return (
-			<Fragment>
-				<Link to="#">
-					<Button className={props.className} color="primary" block onClick={props.onClick}>Sign up</Button>
-				</Link>
-			</Fragment>
-		);
-	// };
+	return (
+		<Fragment>
+			<Link to="#">
+				<Button className={props.className} color="primary" block onClick={props.onClick}>Sign up</Button>
+			</Link>
+		</Fragment>
+	);
 };
 
 export const InProgress = () => {
 	return (
 		<Fragment>
-			<Spinner className="mt-3 mr-2" color="danger" />In progress...
+			<Spinner className="mt-2 mr-2" color="danger" />In progress...
 		</Fragment>
 	);
 };
@@ -34,7 +33,7 @@ export const ResultsPopover = () => {
 				id="results" 
 				type="button" 
 				color="dark"
-				className="mt-3"
+				className="mt-4"
 				block
 			>
 				Results
