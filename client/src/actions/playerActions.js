1eb9116ab48c50a243df7845e3d2e04d47a13d24
Dustin <D.Cint88@gmail.com>
2020-05-10 14:50:40 -0400

    Created show player Action

--- client/src/actions/playerActions.js
+++ client/src/actions/playerActions.js
@@ -1,19 +1,42 @@
-import { GET_PLAYERS, PLAYERS_LOADING } from './types';
+import { 
+	GET_PLAYERS,
+	SHOW_PLAYER,
+	PLAYERS_LOADING, 
+	PLAYER_LOADING
+} from './types';
 import axios from 'axios';
 import { returnErrors } from './errorActions';
 
 export const getPlayers = () => dispatch => {
-	dispatch(setPlayersLoading());
-	axios.get('/users')
-	.then(res => dispatch({
-		type: GET_PLAYERS,
-		payload: res.data
-	}))
-	.catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
+	dispatch(allPlayersLoading());
+	axios
+		.get('/users')
+		.then(res => dispatch({
+			type: GET_PLAYERS,
+			payload: res.data
+		}))
+		.catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
 };
 
-export const setPlayersLoading = () => {
+export const showPlayer = id => dispatch => {
+	dispatch(onePlayerLoading());
+	axios
+		.get(`/users/${id}`)
+		.then(() => dispatch({
+			type: SHOW_PLAYER,
+			payload: id
+		}))
+		.catch(err => dispatch(returnErrors(err.response.data, err.response.status)));
+};
+
+export const allPlayersLoading = () => {
 	return {
 		type: PLAYERS_LOADING
+	}
+};
+
+export const onePlayerLoading = () => {
+	return {
+		type: PLAYER_LOADING
 	}
 };
