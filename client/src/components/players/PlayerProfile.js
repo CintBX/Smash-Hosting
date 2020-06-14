45646057c2fe975c8a026054df8903c013f291f9
Dustin <D.Cint88@gmail.com>
2020-06-14 18:34:56 -0400

    Cheapfixed Profile Page render bug

--- client/src/components/players/PlayerProfile.js
+++ client/src/components/players/PlayerProfile.js
@@ -4,41 +4,44 @@ import { connect } from 'react-redux';
 import { showPlayer } from '../../actions/playerActions';
 
 class PlayerProfile extends Component {
-	// componentDidMount() {
-	// 	this.props.showPlayer(this.props.auth.user._id);
-	// }
-	
 	static propTypes = {
-		// showPlayer: PropTypes.func.isRequired,
-		player: PropTypes.object.isRequired
+		player: PropTypes.object.isRequired,
+		auth: PropTypes.object.isRequired
 	};
 
 	render() {
 		const { username, main, secondary, friendCode, date, tournamentsPlayed } = this.props.player.showPlayer;
+		const { isAuthenticated } = this.props.auth;
 
 		return (
 			<div>
-				<h1 className="text-center">User Profile Page</h1>
-					
-				<h2>Username: {username}</h2>
-				<h3>Main: {main}</h3>
-				<h3>Secondary: {secondary}</h3>
-				<h4>Friend Code: {friendCode}</h4>
-				<h5>Edit Details</h5>
-				<p>Member Since: {date}</p>
-				<br/>
+			{
+				isAuthenticated ?
+				<div>
+					<h1 className="text-center">User Profile Page</h1>
+						
+					<h2>Username: {username}</h2>
+					<h3>Main: {main}</h3>
+					<h3>Secondary: {secondary}</h3>
+					<h4>Friend Code: {friendCode}</h4>
+					<h5>Edit Details</h5>
+					<p>Member Since: {date}</p>
+					<br/>
 
-				<h2>Tournament History</h2>
-				<p>Tounaments Won: </p>
-				<p>Matches won: </p>
-				<p>Matches lost: </p>
+					<h2>Tournament History</h2>
+					<p>Tounaments Won: </p>
+					<p>Matches won: </p>
+					<p>Matches lost: </p>
+				</div> :
+				<p>Sorry! You do not have access to this page</p>
+			}
 			</div>
 		)
 	}
 };
 
 const mapStateToProps = state => ({
-	// auth: state.auth,
+	auth: state.auth,
 	player: state.player
 });
 
