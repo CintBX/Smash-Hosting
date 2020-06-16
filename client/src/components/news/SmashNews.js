2e68034ed6ec471e728ea7e9d917adeae32b5393
Dustin <D.Cint88@gmail.com>
2020-06-16 18:15:17 -0400

    Links now open new tab

--- client/src/components/news/SmashNews.js
+++ client/src/components/news/SmashNews.js
@@ -35,36 +35,36 @@ export default class SmashNews extends React.Component {
 				<h1 className="text-center">Smash News</h1>
 				<ul>
 					<li>
-						<a href="https://www.smashbros.com/en_US/index.html">
+						<a href="https://www.smashbros.com/en_US/index.html" target="_blank">
 							Smash Main Site
 						</a>
 					</li>
 					<li>
-						<a href="https://www.express.co.uk/entertainment/gaming/1212564/Nintendo-Switch-news-Super-Smash-Bros-update-Fighters-Pass-DLC-character-5">
+						<a href="https://www.express.co.uk/entertainment/gaming/1212564/Nintendo-Switch-news-Super-Smash-Bros-update-Fighters-Pass-DLC-character-5" target="_blank">
 							DLC News
 						</a>
 					</li>
 					<li>
-						<a href="https://www.newsweek.com/smash-ultimate-version-6-0-patch-notes-fighter-adjustments-1470128">
+						<a href="https://www.newsweek.com/smash-ultimate-version-6-0-patch-notes-fighter-adjustments-1470128" target="_blank">
 							Latest Patch Notes
 						</a>
 					</li>
 					<li>
-						<a href="https://www.proguides.com/super-smash-bros-ultimate/characters/">
+						<a href="https://www.proguides.com/super-smash-bros-ultimate/characters/" target="_blank">
 							Tier List
 						</a>
 					</li>
 					<li>
-						<a href="https://smash.gg/tournament/esports-arena-smash-ultimate-tournament-sponsored-by-progressive/events">
+						<a href="https://smash.gg/tournament/esports-arena-smash-ultimate-tournament-sponsored-by-progressive/events" target="_blank">
 							ESports Schedule
 						</a>
 					</li>
 					<li>
 						Professional Smash<br/>
-						<a href="https://www.youtube.com/user/VideoGameBootCamp/videos">
+						<a href="https://www.youtube.com/user/VideoGameBootCamp/videos" target="_blank">
 							VGBootCamp
 						</a><br/>
-						<a href="https://www.youtube.com/channel/UC_dkX2_MJZeiYZImFN8AOvg">
+						<a href="https://www.youtube.com/channel/UC_dkX2_MJZeiYZImFN8AOvg" target="_blank">
 							MostValuableGaming
 						</a>
 					</li>
