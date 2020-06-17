a003cadcc6576e549bcdecb8534add5559c6551a
Dustin <D.Cint88@gmail.com>
2020-06-17 15:00:48 -0400

    Color change Tier List

--- client/src/components/news/SmashNews.js
+++ client/src/components/news/SmashNews.js
@@ -116,7 +116,7 @@ export default class SmashNews extends React.Component {
 					</div>
 
 					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-						<div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+						<div className="bg-warning mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
 							<div className="my-3 py-3">
 								<h2 className="display-5">Current Tier List</h2>
 								<p className="lead">Character rankings based on performance in professional competition</p>
