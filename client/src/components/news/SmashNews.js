fc476e04f3c6782dcaa1cccf38907a137990a596
Dustin <D.Cint88@gmail.com>
2020-06-17 14:02:02 -0400

    Initial style for Tier List

--- client/src/components/news/SmashNews.js
+++ client/src/components/news/SmashNews.js
@@ -3,6 +3,7 @@ import '../../css/smashNewsStyles.css';
 import SmashMain from '../../img/smashNews/SmashMain.png';
 import DLCnews from '../../img/smashNews/DLCnews.png';
 import PatchNotes from '../../img/smashNews/PatchNotes.png';
+import TierList from '../../img/smashNews/TierList.png';
 
 // const items = [
 // 	{
@@ -110,6 +111,18 @@ export default class SmashNews extends React.Component {
 							</div>
 							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
 								<img src={ PatchNotes } style={{width: '100%', height: '100%'}}/>
+							</div>
+						</div>
+					</div>
+
+					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+						<div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+							<div className="my-3 py-3">
+								<h2 className="display-5">Current Tier List</h2>
+								<p className="lead">Character rankings based on performance in professional competition</p>
+							</div>
+							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
+								<img src={ TierList } style={{width: '100%', height: '100%'}}/>
 							</div>
 						</div>
 					</div>
