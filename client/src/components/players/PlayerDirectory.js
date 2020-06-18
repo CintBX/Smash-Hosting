47994aada4951e5ddfb6653f14ae7503b1f8ca92
Dustin <D.Cint88@gmail.com>
2020-06-17 21:13:19 -0400

    Player Count and Alphabetical order

--- client/src/components/players/PlayerDirectory.js
+++ client/src/components/players/PlayerDirectory.js
@@ -23,28 +23,51 @@ class PlayerDirectory extends Component {
 	render() {
 		const { players } = this.props.player;
 
-		return players.map(({ _id, username, main, secondary, friendCode }) => {
-			return (
-				<div>
-					<Link to={`/player/${_id}`}>
-						<Media className="media-element" onClick={this.onShowPlayer.bind(this, _id)}>
-								<Media left href="#">
-									<Media object src="https://via.placeholder.com/50x50" />
-								</Media>
-								<Media body>
-									<Media heading className="ml-2">
-										<b>{ username }</b>
-									</Media>
-									<i style={{fontSize:"0.9rem"}}>
-										<Media className="ml-2">{ main } / { secondary } </Media>
-										<Media className="ml-2">{ friendCode }</Media>
-									</i>
-								</Media>
-						</Media><br/>
-					</Link>
-				</div>
-			)
-		})
+		const alphabeticalOrder = (a, b) => {
+			const userA = a.username.toLowerCase();
+			const userB = b.username.toLowerCase();
+
+			let comparison = 0;
+			if (userA > userB) {
+				comparison = 1;
+			} else if (userA < userB) {
+				comparison = -1;
+			}
+
+			return comparison;
+		}
+
+		players.sort(alphabeticalOrder);
+		
+		return (
+			<div>
+				<h1>Registered Fighters ({players.length})</h1><br/>
+				{
+					players.map(({ _id, username, main, secondary, friendCode }) => {
+						return (
+							<div>
+								<Link to={`/player/${_id}`}>
+									<Media className="media-element" onClick={this.onShowPlayer.bind(this, _id)}>
+										<Media left href="#">
+											<Media object src="https://via.placeholder.com/50x50" />
+										</Media>
+										<Media body>
+											<Media heading className="ml-2">
+												<b>{ username }</b>
+											</Media>
+											<i style={{fontSize:"0.9rem"}}>
+												<Media className="ml-2">{ main } / { secondary } </Media>
+												<Media className="ml-2">{ friendCode }</Media>
+											</i>
+										</Media>
+									</Media><br/>
+								</Link>
+							</div>
+						)
+					})
+				}
+			</div>
+		)
 	}
 };
 
