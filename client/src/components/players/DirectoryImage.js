da070541ad0e21ebb32b643aed5f20e0bd49d9ea
Dustin <D.Cint88@gmail.com>
2020-06-19 20:24:43 -0400

    Implemented Series Symbols, Player Directory

--- client/src/components/players/DirectoryImage.js
+++ client/src/components/players/DirectoryImage.js
@@ -3,7 +3,7 @@ import seriesSymbols from '../SeriesSymbols';
 
 function DirectoryImage(props) {
   return seriesSymbols.map(({ id, src, name }) => {
-    if(name === props.name) {
+    if(name === props.main) {
       return <img width="100%" id={id} src={src} name={name} alt="series symbol" />
     };
   });
