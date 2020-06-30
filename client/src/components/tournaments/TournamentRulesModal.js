dc3ba74419d2d2908f543b71bfc89ee1825a3af2
Dustin <D.Cint88@gmail.com>
2020-06-30 13:40:54 -0400

    Initial front end implement

--- client/src/components/tournaments/TournamentRulesModal.js
+++
@@ -1,9 +0,0 @@
-// Create the modal like you did Updte User Details
-// This will replace New.js
-// Use conditional logic to render the same modal, but will be slightly diff depending on
-// whether "Host New Tournament" is selected, or "Change Ruleset" is selected
-
-// The modal will be the same, but the backEnd stuff is what will be different.
-// Begin by doing the backend route, and wire up redux.
-// Host New Tournament will use POST tournaments/new, 
-// while Change Ruleset will use POST/UPDATE tournaments/update
