02b59765fb588194c2874f603ab59cd8a3765e9c
Dustin <D.Cint88@gmail.com>
2020-07-15 20:17:48 -0400

    added GET all tourneys to update Tourney

--- client/src/actions/tournamentActions.js
+++ client/src/actions/tournamentActions.js
@@ -70,6 +70,13 @@ export const updateTournament = ({ _id, title }) => dispatch => {
 		.catch(err => {
 			dispatch(returnErrors(err.response.data, err.response.status));
 		});
+
+	axios
+		.get('/tournaments')
+		.then(res => dispatch({
+			type: GET_TOURNAMENTS,
+			payload: res.data
+		}));
 };
 
 export const deleteTournament = id => (dispatch, getState) => {
