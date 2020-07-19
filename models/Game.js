c316e692c4bde1167d7443a96a38af74bb1f453a
Dustin <D.Cint88@gmail.com>
2020-07-19 18:09:29 -0400

    Initial Game model complete

---
+++ models/Game.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+  format: {
+    type: String,
+    required: true
+  },
+  participants: {
+    type: Array,
+    required: true
+  },
+  round: {
+    number: {
+      Type: Number,
+      required: true,
+    },
+    matches: {
+      type: Array,
+    },
+    semiFinal: {
+      type: Array,
+    },
+    final: {
+      type: Array,
+    },
+  },
+  winner: {
+    type: String,
+  }
+});
