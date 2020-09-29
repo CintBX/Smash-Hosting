3e4dba1a700988e818d6ff4d5870a3485591f23d
Dustin <D.Cint88@gmail.com>
2020-09-29 13:45:38 -0400

    Forced image height to match

--- client/src/components/layout/SidebarImage.js
+++ client/src/components/layout/SidebarImage.js
@@ -4,7 +4,7 @@ import { fullRoster } from '../Characters';
 function SidebarImage(props) {
 		return fullRoster.map(({ id, src, name }) => {
 		if(name === props.main) {			
-			return <img width="100%" id={id} src={src} name={name} alt="main character" />
+			return <img width="100%" height="300px" id={id} src={src} name={name} alt="main character" />
 		};
 	});
 };
