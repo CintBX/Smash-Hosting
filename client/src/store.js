4c2352aacfdaa80c2e7e7bd0d0f60f7642c76b3b
Dustin <D.Cint88@gmail.com>
2020-10-03 15:33:00 -0400

    Issue solved - reorganized store.js

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-// const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
-
 const store = createStore(
 	rootReducer,
 	initialState,
-	compose(applyMiddleware(...middleware))
+	compose(
+		applyMiddleware(...middleware)
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;
-
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
