15fe381a12a51241e4950380058637a8744ebd00
Dustin <D.Cint88@gmail.com>
2020-10-03 12:04:59 -0400

    Ignore and rmcache node_modules

--- node_modules/react-tournament-bracket/lib/components/model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Side;
-(function (Side) {
-    Side["HOME"] = "home";
-    Side["VISITOR"] = "visitor";
-})(Side = exports.Side || (exports.Side = {}));
--- node_modules/react-tournament-bracket/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Bracket from './components/Bracket';
-import BracketGame from './components/BracketGame';
-import BracketGenerator from './components/BracketGenerator';
-import * as Model from './components/model';
-export { Bracket, BracketGame, BracketGenerator, Model };
