340d893e36fcaf93ccd77c757f21642ac8bf6618
Dustin <D.Cint88@gmail.com>
2020-10-04 22:39:35 -0400

    Refactored series symbols and render

--- client/src/components/players/DirectoryImage.js
+++ client/src/components/players/DirectoryImage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import seriesSymbols from '../SeriesSymbols';
+import { seriesSymbols } from '../SeriesSymbols';
 
 function DirectoryImage(props) {
-  return seriesSymbols.map(({ src, name }) => {
-    if(name === props.main) {
-      return <img width="100%" src={src} name={name} alt="series symbol" />
+  return seriesSymbols.map(({ src, characters }) => {
+    if(characters.includes(props.main)) {
+      return <img width="100%" src={src} alt="series symbol" />
     };
   });
 };
