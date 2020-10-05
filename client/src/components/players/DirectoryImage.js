25cee79a1d3e3f86d7213f130dc4e7cace6b6f29
Dustin <D.Cint88@gmail.com>
2020-10-04 21:55:44 -0400

    Removed id from series symbols

--- client/src/components/players/DirectoryImage.js
+++ client/src/components/players/DirectoryImage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import seriesSymbols from '../SeriesSymbols';
 
 function DirectoryImage(props) {
-  return seriesSymbols.map(({ id, src, name }) => {
+  return seriesSymbols.map(({ src, name }) => {
     if(name === props.main) {
-      return <img width="100%" id={id} src={src} name={name} alt="series symbol" />
+      return <img width="100%" src={src} name={name} alt="series symbol" />
     };
   });
 };
