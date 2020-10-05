4d0ae36615d21e27e410c172aa822a87dae0f5ca
Dustin <D.Cint88@gmail.com>
2020-10-04 21:39:03 -0400

    Updated roster including random and DLC

--- client/src/components/layout/SidebarImage.js
+++ client/src/components/layout/SidebarImage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { fullRoster } from '../Characters';
 
 function SidebarImage(props) {
-		return fullRoster.map(({ id, src, name }) => {
+		return fullRoster.map(({ src, name }) => {
 		if(name === props.main) {			
-			return <img width="100%" height="300px" id={id} src={src} name={name} alt="main character" />
+			return <img width="100%" height="300px" src={src} name={name} alt="main character" />
 		};
 	});
 };
