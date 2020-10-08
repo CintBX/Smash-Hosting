e8856ac79d983cc12fd02cfa6b908a4bc2c65138
Dustin <D.Cint88@gmail.com>
2020-10-08 10:42:01 -0400

    Temp Solution - Valid routes

--- client/src/components/news/SmashNews.js
+++ client/src/components/news/SmashNews.js
@@ -8,65 +8,63 @@ import TierList from '../../img/smashNews/TierList.png';
 export default class SmashNews extends Component {
 	render() {
 		return (
-			<div>
-				<body className="smash-news">
-					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-						<div className="bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-							<div className="my-3 py-3">
-								<h2 className="display-5">Smash Ultimate Main</h2>
-								<p className="lead">The official website for Super Smash Bros. Ultimate</p>
-							</div>
-							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
-								<a href="https://www.smashbros.com/en_US/index.html" target="_blank" rel="noopener noreferrer">
-									<img src={ SmashMain } style={{ width: '100%', height: '100%' }} alt="smash ultimate main page" />
-								</a>
-							</div>
+			<body className="smash-news">
+				<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+					<div className="bg-danger mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+						<div className="my-3 py-3">
+							<h2 className="display-5">Smash Ultimate Main</h2>
+							<p className="lead">The official website for Super Smash Bros. Ultimate</p>
+						</div>
+						<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
+							<a href="https://www.smashbros.com/en_US/index.html" target="_blank" rel="noopener noreferrer">
+								<img src={ SmashMain } style={{ width: '100%', height: '100%' }} alt="smash ultimate main page" />
+							</a>
 						</div>
 					</div>
+				</div>
 
-					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-						<div className="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-							<div className="my-3 py-3">
-								<h2 className="display-5">Downloadable Content</h2>
-								<p className="lead">Click here for news on the latest DLC</p>
-							</div>
-							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
-								<a href="https://smashbros.nintendo.com/buy/dlc/" target="_blank" rel="noopener noreferrer">
-									<img src={ DLCnews } style={{ width: '100%', height: '100%' }} alt="DLC news" />
-								</a>
-							</div>
+				<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+					<div className="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+						<div className="my-3 py-3">
+							<h2 className="display-5">Downloadable Content</h2>
+							<p className="lead">Click here for news on the latest DLC</p>
+						</div>
+						<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
+							<a href="https://smashbros.nintendo.com/buy/dlc/" target="_blank" rel="noopener noreferrer">
+								<img src={ DLCnews } style={{ width: '100%', height: '100%' }} alt="DLC news" />
+							</a>
 						</div>
 					</div>
+				</div>
 
-					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-						<div className="bg-success mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-							<div className="my-3 py-3">
-								<h2 className="display-5">Patch Notes</h2>
-								<p className="lead">All changes to your favorite characters and more</p>
-							</div>
-							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
-								<a href="https://en-americas-support.nintendo.com/app/answers/detail/a_id/49877" target="_blank" rel="noopener noreferrer">
-									<img src={ PatchNotes } style={{ width: '100%', height: '100%' }} alt="latest patch notes" />
-								</a>
-							</div>
+				<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+					<div className="bg-success mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+						<div className="my-3 py-3">
+							<h2 className="display-5">Patch Notes</h2>
+							<p className="lead">All changes to your favorite characters and more</p>
+						</div>
+						<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
+							<a href="https://en-americas-support.nintendo.com/app/answers/detail/a_id/49877" target="_blank" rel="noopener noreferrer">
+								<img src={ PatchNotes } style={{ width: '100%', height: '100%' }} alt="latest patch notes" />
+							</a>
 						</div>
 					</div>
+				</div>
 
-					<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-						<div className="bg-secondary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-							<div className="my-3 py-3">
-								<h2 className="display-5">Current Tier List</h2>
-								<p className="lead">Character rankings based on performance in professional competition</p>
-							</div>
-							<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
-								<a href="https://www.proguides.com/super-smash-bros-ultimate/characters/" target="_blank" rel="noopener noreferrer">
-									<img src={ TierList } style={{ width: '100%', height: '100%' }} alt="current tier list" />
-								</a>
-							</div>
+				<div className="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+					<div className="bg-secondary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+						<div className="my-3 py-3">
+							<h2 className="display-5">Current Tier List</h2>
+							<p className="lead">Character rankings based on performance in professional competition</p>
+						</div>
+						<div className="bg-light shadow-sm mx-auto" style={{width: '90%', height: '300px', borderRadius: '21px 21px 0 0'}}>
+							<a href="https://www.proguides.com/super-smash-bros-ultimate/characters/" target="_blank" rel="noopener noreferrer">
+								<img src={ TierList } style={{ width: '100%', height: '100%' }} alt="current tier list" />
+							</a>
 						</div>
 					</div>
-				</body>
-			</div>
+				</div>
+			</body>
 		)
 	}
 };
