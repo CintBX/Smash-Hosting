b8b07bf419ab04a3d67fbcc5ea1f85a898979ddc
Dustin <D.Cint88@gmail.com>
2020-10-13 00:45:09 -0400

    Added loading spinner to Profile

--- client/src/components/players/PlayerProfile.js
+++ client/src/components/players/PlayerProfile.js
@@ -5,6 +5,7 @@ import { showPlayer } from '../../actions/playerActions';
 import { deleteAccount } from '../../actions/authActions';
 import moment from 'moment';
 import DeleteModal from '../delete/DeleteModal';
+import { Spinner } from 'reactstrap';
 
 class PlayerProfile extends Component {
 	constructor(props) {
@@ -41,31 +42,36 @@ class PlayerProfile extends Component {
 		const { isAuthenticated, user } = this.props.auth;
 
 		return (
-			<div className="text-center mb-5" style={{color: "#56A8CBFF"}}>
-				<h1>{ username }</h1><hr />
+			<div>
+				{ this.props.player.loading ?
+					<Spinner color="light" /> :
+					<div className="text-center mb-5" style={{color: "#56A8CBFF"}}>
+						<h1>{ username }</h1><hr />
 
-				<h4>
-					Plays: { main && secondary ? main + ", " + secondary : main || secondary || "No characters selected" }
-				</h4>
-				<h5>Code: {friendCode}</h5>
-				<h5>Member since {moment(date).format("dddd, MMMM Do YYYY")}</h5>
-				
-				<br/><br/>
-				<span>
-					<h2>History</h2>
-					<h5>Tounaments Participated in: {tournamentsPlayed}</h5>
-					<h5>Tournaments won: {tournamentWins}</h5>
-					<h5>Matches won: {matchWins}</h5><br />
-				</span>
+						<h4>
+							Plays: { main && secondary ? main + ", " + secondary : main || secondary || "No characters selected" }
+						</h4>
+						<h5>Code: {friendCode}</h5>
+						<h5>Member since {moment(date).format("dddd, MMMM Do YYYY")}</h5>
+						
+						<br/><br/>
+						<span>
+							<h2>History</h2>
+							<h5>Tounaments Participated in: {tournamentsPlayed}</h5>
+							<h5>Tournaments won: {tournamentWins}</h5>
+							<h5>Matches won: {matchWins}</h5><br />
+						</span>
 
-				{
-					isAuthenticated && user.username === username ?
-					<DeleteModal 
-						page={"Player Profile"} 
-						title={"Delete your account"} 
-						onClick={() => this.onDelete(user._id)} 
-					/> :
-					null
+						{
+							isAuthenticated && user.username === username ?
+							<DeleteModal 
+								page={"Player Profile"} 
+								title={"Delete your account"} 
+								onClick={() => this.onDelete(user._id)} 
+							/> :
+							null
+						}
+					</div>
 				}
 			</div>
 		)
