fcdd0721fdd204f7dab412c7f7d6b5c5a32b9538
Dustin <D.Cint88@gmail.com>
2020-10-12 23:49:42 -0400

    Added loading spinner to tournament Show

--- client/src/components/tournaments/Show.js
+++ client/src/components/tournaments/Show.js
@@ -5,7 +5,7 @@ import { showTournament, addParticipant, updateTournamentStatus } from '../../ac
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { TournamentSignUp, StartTournament } from './buttons';
-import { Button } from 'reactstrap';
+import { Button, Spinner } from 'reactstrap';
 
 class TournamentShow extends Component {
 	constructor(props) {
@@ -37,59 +37,64 @@ class TournamentShow extends Component {
 		const { isAuthenticated, user } = this.props.auth;
 
 		return (
-			<div style={{color: "lightgrey"}}>
-				<h1 className="text-center">
-					{ title }
-					<span style={{fontSize: "0.5em"}}> by { hostedBy }</span>
-				</h1>
+			<div>
+				{ this.props.tournament.loading ?
+					<Spinner color="light" /> :
+					<div style={{color: "lightgrey"}}>
+						<h1 className="text-center">
+							{ title }
+							<span style={{fontSize: "0.5em"}}> by { hostedBy }</span>
+						</h1>
 
-				<h3>
-					<TournamentDescription key={_id} title={ title } />
-				</h3>
-				
-				<br />
+						<h3>
+							<TournamentDescription key={_id} title={ title } />
+						</h3>
+						
+						<br />
 
-				<p className="text-center" style={{color: "#56A8CBFF", fontSize: "2em"}}>
-					~ { status } for registration ~
-				</p>
+						<p className="text-center" style={{color: "#56A8CBFF", fontSize: "2em"}}>
+							~ { status } for registration ~
+						</p>
 
-				<h4 className="text-left mt-5">
-					{
-						participants && participants.length === 1 ? 
-						`${participants && participants.length} Registered Fighter` : 
-						`${participants && participants.length} Registered Fighters`
-					}
-				</h4>
+						<h4 className="text-left mt-5">
+							{
+								participants && participants.length === 1 ? 
+								`${participants && participants.length} Registered Fighter` : 
+								`${participants && participants.length} Registered Fighters`
+							}
+						</h4>
 
-				<ul>
-					{
-						participants && participants.map(participant => (
-							<li key={participant._id} className="text-left" style={{fontSize: "1.1em"}}>{participant.username}</li>
-						))
-					}
-				</ul>
+						<ul>
+							{
+								participants && participants.map(participant => (
+									<li key={participant._id} className="text-left" style={{fontSize: "1.1em"}}>{participant.username}</li>
+								))
+							}
+						</ul>
 
-				{
-					isAuthenticated ?
-					<div>
-						<TournamentSignUp
-							participants={participants}
-							userId={user._id}
-							onClick={() => this.onSignUp(_id, user)} 
-						/>
-					</div> :
-					<Button block disabled>Log in to sign up for this tournament</Button>
-				}
+						{
+							isAuthenticated ?
+							<div>
+								<TournamentSignUp
+									participants={participants}
+									userId={user._id}
+									onClick={() => this.onSignUp(_id, user)} 
+								/>
+							</div> :
+							<Button block disabled>Log in to sign up for this tournament</Button>
+						}
 
-				{
-					isAuthenticated && user.username === hostedBy ?
-					<div>
-						<StartTournament
-							participants={participants}
-							onClick={() => this.onStartTournament(_id, participants)}
-						/>
-					</div> :
-					null
+						{
+							isAuthenticated && user.username === hostedBy ?
+							<div>
+								<StartTournament
+									participants={participants}
+									onClick={() => this.onStartTournament(_id, participants)}
+								/>
+							</div> :
+							null
+						}
+					</div>
 				}
 				<br /><Link to="/">Back to Tournaments main page</Link>
 			</div>
