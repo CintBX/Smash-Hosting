c88e5b9695b47e0bf1ed1e52216c4d7a43563c03
Dustin <D.Cint88@gmail.com>
2020-10-21 11:11:18 -0400

    Brackets for 8 and 9 players created

---
+++ client/src/components/tournaments/TempSwitch.js
@@ -0,0 +1,347 @@
+// 8 Players --------------------------------------------------------
+const game = {
+  id: 1,
+  scheduled: null,
+  name: "Finals",
+  sides: {
+    home: {
+      score: { score: 0 },
+      seed: {
+        sourceGame: {
+          id: 2,
+          scheduled: null,
+          name: "B1",
+          sides: {
+            home: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "A1",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 1,
+                        displayName: "Seed 0"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 2,
+                        displayName: "Seed 1"
+                      }
+                    }
+                  }
+                },
+                rank: 1,
+                displayName: "Winner of A1"
+              }
+            },
+            visitor: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "A2",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 3,
+                        displayName: "Seed 2"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 4,
+                        displayName: "Seed 3"
+                      }
+                    }
+                  }
+                },
+                rank: 1,
+                displayName: "Winner of A2"
+              }
+            }
+          }
+        },
+        rank: 1,
+        displayName: "Winner of B1"
+      }
+    },
+    visitor: {
+      score: { score: 0 },
+      seed: {
+        sourceGame: {
+          id: 2,
+          scheduled: null,
+          name: "B2",
+          sides: {
+            home: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "A3",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 4"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 5"
+                      }
+                    }
+                  },
+                },
+                rank: 1,
+                displayName: "Winner of A3"
+              }
+            },
+            visitor: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "A4",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 6"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 8,
+                        displayName: "Seed 7"
+                      }
+                    }
+                  },
+                },
+                rank: 1,
+                displayName: "Winner of A4"
+              }
+            }
+          }
+        },
+        rank: 1,
+        displayName: "Winner of B2"
+      }
+    }
+  }
+}
+
+
+// 9 Players --------------------------------------------------------
+const game = {
+  id: 1,
+  scheduled: null,
+  name: "Finals",
+  sides: {
+    home: {
+      score: { score: 0 },
+      seed: {
+        sourceGame: {
+          id: 2,
+          scheduled: null,
+          name: "C1",
+          sides: {
+            home: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "B1",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: {
+                          id: 4,
+                          scheduled: null,
+                          name: "A1",
+                          sides: {
+                            home: {
+                              score: { score: 0 },
+                              seed: {
+                                sourceGame: null,
+                                rank: 1,
+                                displayName: "Seed 0"
+                              }
+                            },
+                            visitor: {
+                              score: { score: 0 },
+                              seed: {
+                                sourceGame: null,
+                                rank: 1,
+                                displayName: "Seed 1"
+                              }
+                            }
+                          }
+                        },
+                        rank: 1,
+                        displayName: "Winner of A1"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 2,
+                        displayName: "Seed 2"
+                      }
+                    }
+                  }
+                },
+                rank: 1,
+                displayName: "Winner of B1"
+              }
+            },
+            visitor: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "B2",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 3,
+                        displayName: "Seed 3"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 4,
+                        displayName: "Seed 4"
+                      }
+                    }
+                  }
+                },
+                rank: 1,
+                displayName: "Winner of B2"
+              }
+            }
+          }
+        },
+        rank: 1,
+        displayName: "Winner of C1"
+      }
+    },
+    visitor: {
+      score: { score: 0 },
+      seed: {
+        sourceGame: {
+          id: 2,
+          scheduled: null,
+          name: "C2",
+          sides: {
+            home: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "B3",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 5"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 6"
+                      }
+                    }
+                  },
+                },
+                rank: 1,
+                displayName: "Winner of B3"
+              }
+            },
+            visitor: {
+              score: { score: 0 },
+              seed: {
+                sourceGame: {
+                  id: 3,
+                  scheduled: null,
+                  name: "B4",
+                  sides: {
+                    home: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 7,
+                        displayName: "Seed 7"
+                      }
+                    },
+                    visitor: {
+                      score: { score: 0 },
+                      seed: {
+                        sourceGame: null,
+                        rank: 8,
+                        displayName: "Seed 8"
+                      }
+                    }
+                  },
+                },
+                rank: 1,
+                displayName: "Winner of B4"
+              }
+            }
+          }
+        },
+        rank: 1,
+        displayName: "Winner of C2"
+      }
+    }
+  }
+}
+
+
+// 10 Players --------------------------------------------------------
+// pending
+
+// 11 Players
+// 12 Players
+// 13 Players
+// 14 Players
+// 15 Players
+// 16 Players
