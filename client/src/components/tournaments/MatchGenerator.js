bb0cc4c335e9087f1db9a5f914f112030f929c1a
Dustin <D.Cint88@gmail.com>
2020-11-03 11:03:18 -0500

    Clear comments

--- client/src/components/tournaments/MatchGenerator.js
+++ client/src/components/tournaments/MatchGenerator.js
@@ -16,7 +16,6 @@ class MatchGenerator extends Component {
     super(props);
     console.log(props.players);
     this.setPlayersIntoPairs = this.setPlayersIntoPairs.bind(this);
-    this.config8 = this.config8.bind(this);
   };
 
   setPlayersIntoPairs(players) {
@@ -30,16 +29,6 @@ class MatchGenerator extends Component {
     };
     return pairs;
 	};
-	
-	config8() {
-		const { players } = this.props.tournament.showTournament.bracket;
-		let array = [];
-		
-    const pairedPlayers = this.onSetPlayersIntoPairs(players);
-    pairedPlayers.forEach(pair => array.push(pair));
-		
-		return array;
-  };
 
   render() {
     // const { currentRound } = this.state;
@@ -98,32 +87,3 @@ class MatchGenerator extends Component {
 };
 
 export default MatchGenerator;
-
-/*
-<Card body outline color="danger" className="match-card">
-  Each one: Name, Score, Win button
-  <CardTitle className="match-card-title">
-    <span className="float-left match-card-player">{pair[0].username}</span> 
-    <span className="float-right match-card-player">{pair[1].username}</span>
-  </CardTitle>
-
-    Depending on BO3/Bo5, incrementor will max at 2 or 3, and
-    decrementor will min at 0
-
-  <CardText>
-    <span className="float-left match-card-score">- 0 +</span>
-    <span className="float-right match-card-score">- 0 +</span>
-  </CardText>
-
-    I was wrong. THIS is User.matchWins.  The incrementor is for rounds per match.
-
-    One button each player
-    each will have a function(user) that will be take that user as a param
-    Sent to backend for matchWins update, and update bracket.matches/rounds
-
-  <CardBody>
-    <Button className="float-left match-card-btn" color="danger">Winner</Button>
-    <Button className="float-right match-card-btn" color="danger">Winner</Button>
-  </CardBody>
-</Card>
-*/
