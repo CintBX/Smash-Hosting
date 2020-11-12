01c827d65189ab511a3a2a453f9e498a2f0f1b7e
Dustin Cintron <D.Cint88@gmail.com>
2020-11-12 10:59:03 -0500

    Removed switchcase from MatchGen - not needed

--- client/src/components/tournaments/MatchGenerator.js
+++ client/src/components/tournaments/MatchGenerator.js
@@ -18,9 +18,9 @@ class MatchGenerator extends Component {
 
   render() {
     const {
-      round,   //currentRound.round#
-      matches, //currentRound.matches[]
-      finals,  //currentRound.finals?
+      round,
+      matches,
+      finals,
       onSetWinner,
       winners,
       onSetNextRound,
@@ -29,92 +29,39 @@ class MatchGenerator extends Component {
 
     const pairs = onSetPlayersIntoPairs(matches);
 
-    switch(this.props.bracketSize) {
-      // case 20:
-      // case 19:
-      // case 18:
-      // case 17:
-      // case 16:
-      // case 15:
-      // case 14:
-      // case 13:
-      // case 12:
-      // case 11:
-      // case 10:
-      case 9:
-        // I get it, I can't handle this here.  I need to pass it down from TournamentShow
-        return (
-          <Fragment>
-            <MatchCards
-              round={round}
-              finals={finals}
-              onSetWinner={onSetWinner}
-              pairs={pairs}
-              winners={winners}
-            />
+    return (
+      <Fragment>
+        <MatchCards
+          round={round}
+          finals={finals}
+          onSetWinner={onSetWinner}
+          pairs={pairs}
+          winners={winners}
+        />
 
-            <div style={{color: "lightgrey"}}>
-              <h3>
-                Round Winners
-                <span style={{fontSize:"0.8em"}}> (refresh if incorrect)</span>
-              </h3>
-              <ul>
-                {
-                  winners && winners.map(winner => (
-                    <li>{winner.username}</li>
-                  ))
-                }
-              </ul>
-            </div>
+        <div style={{color: "lightgrey"}}>
+          <h3>
+            Round Winners
+            <span style={{fontSize:"0.8em"}}> (refresh if incorrect)</span>
+          </h3>
+          <ul>
+            {
+              winners && winners.map(winner => (
+                <li>{winner.username}</li>
+              ))
+            }
+          </ul>
+        </div>
 
-            <Button 
-              block 
-              color="primary"
-              onClick={onSetNextRound}
-            >
-              Proceed to the Next Round
-            </Button>
-          </Fragment>
-        );
-
-      case 8:
-        return (
-          <Fragment>
-            <MatchCards
-              round={round}
-              finals={finals}
-              onSetWinner={onSetWinner}
-              pairs={pairs}
-              winners={winners}
-            />
-
-            <div style={{color: "lightgrey"}}>
-              <h3>
-                Round Winners
-                <span style={{fontSize:"0.8em"}}> (refresh if incorrect)</span>
-              </h3>
-              <ul>
-                {
-                  winners && winners.map(winner => (
-                    <li>{winner.username}</li>
-                  ))
-                }
-              </ul>
-            </div>
-
-            <Button 
-              block 
-              color="primary"
-              onClick={onSetNextRound}
-            >
-              Proceed to the Next Round
-            </Button>
-          </Fragment>
-        );
-
-      default:
-        return <div>No matches available</div>;
-    };
+        <Button 
+          block 
+          color="primary"
+          onClick={onSetNextRound}
+        >
+          Proceed to the Next Round
+        </Button>
+      </Fragment>
+    );
   };
 };
 
