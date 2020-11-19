40c70df9b17a2047f0881b9dba262bfb01eaacb7
Dustin Cintron <D.Cint88@gmail.com>
2020-11-19 17:12:02 -0500

    Results page

--- client/src/components/tournaments/TournamentScreens.js
+++ client/src/components/tournaments/TournamentScreens.js
@@ -2,10 +2,20 @@ import React from 'react';
 import moment from 'moment';
 import { TournamentSignUp, StartTournament } from './resources/buttons';
 import { TournamentRules } from './resources/rulesets';
-import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import MatchGenerator from './MatchGenerator';
 import BracketGenerator from './BracketGenerator';
+import {
+	Media,
+	Card,
+	CardText,
+	CardBody,
+	CardTitle,
+	Button
+} from 'reactstrap';
+import DirectoryImage from '../players/DirectoryImage';
+import { fullRoster } from '../Characters';
+import logo from '../../logo.svg.png';
 
 // Status === "Open"
 export const SignUpPage = ({ tournament, auth, onSignUp, onStartTournament }) => {
@@ -142,17 +152,173 @@ export const StartBracket = ({ tournament }) => {
 
 
 // Status === "Complete"
+const WinnerImage = props => {
+	return fullRoster.map(({ src, name }) => {
+		if(name === props.main) {			
+			return <img width="100%" src={src} name={name} alt="main character" />
+		} else {
+			return null;
+		};
+	});
+};
+
 export const Results = ({ tournament }) => {
-	const { participants, bracket } = tournament;
+	const { _id, title, hostedBy, type, participants, bracket } = tournament;
+	const n = bracket.rounds.length;
+	let winner;
+	let secondPlaceWinner;
+	const thirdPlaceWinners = [];
+
+	// 1st place winner
+	winner = bracket.champion[0];
+
+	// 2nd place winner
+	bracket.rounds[n-1].matches.forEach(player => {
+		if(player._id !== winner._id) {
+			secondPlaceWinner = player;
+		};
+	});
+	
+	// 3rd place winners
+	bracket.rounds[n-2].matches.forEach(player => {
+		if(player._id !== winner._id && player._id !== secondPlaceWinner._id) {
+			thirdPlaceWinners.push(player);
+		};
+	});
+
   return (
     <div style={{color:"lightgrey"}}>
-      <h1>Tournament Results</h1>
-			<BracketGenerator
-				bracketSize={participants && participants.length}
-				rounds={bracket.rounds}
-				players={bracket.players}
-				scores={bracket.scores}
-			/>
+      <h1 className="text-center">
+				{ title }
+			</h1>
+
+			<hr style={{backgroundColor:"lightgrey"}} />
+
+			<h4>
+				Rules: { type }
+			</h4>
+
+			<h4>
+				<TournamentRules key={_id} type={ type } />
+			</h4>
+
+			<br/>
+
+			<div>
+				<Card
+					className="text-center"
+					style={{ fontSize:"3em", color:"#DA291CFF", backgroundColor:"#56A8CBFF" }}
+				>
+					<CardBody>
+						<CardTitle className="results-font">
+							<img src={logo} width="60" height="60" alt="Smash Brothers Logo" />
+								<span className="mx-5">1st Place</span>
+							<img src={logo} width="60" height="60" alt="Smash Brothers Logo" />
+						</CardTitle>
+					</CardBody>
+					<WinnerImage main={ winner.main } />
+					<CardBody>
+						<CardText>
+							{ winner.username }
+						</CardText>
+						<Link to={`/player/${winner._id}`} className="remove-underline">
+							<Button style={{fontSize:"0.5em"}} block outline color="danger">
+								View Profile
+							</Button>
+						</Link>
+					</CardBody>
+				</Card>
+			</div>
+
+			<br/>
+
+			<p className="text-center results-font" style={{color: "#56A8CBFF", fontSize: "1.9em"}}>
+				~ 2nd Place Winner ~
+			</p>
+			<p>
+				<Link to={`/player/${secondPlaceWinner._id}`} className="remove-underline">
+					<Media className="media-element media-hover">
+						<Media left>
+							<DirectoryImage key={secondPlaceWinner._id} main={secondPlaceWinner.main} />
+						</Media>
+						<Media body>
+							<Media heading className="ml-3">
+								<b>{ secondPlaceWinner.username }</b>
+							</Media>
+							<i style={{fontSize:"0.9rem"}}>
+								{
+									secondPlaceWinner.main && secondPlaceWinner.secondary ?
+									<Media className="ml-3">{ secondPlaceWinner.main + ", " + secondPlaceWinner.secondary }</Media> :
+									<Media className="ml-3">{ secondPlaceWinner.main || secondPlaceWinner.secondary || null }</Media>
+								}
+								<Media className="ml-3">{ secondPlaceWinner.friendCode }</Media>
+							</i>
+						</Media>
+					</Media>
+				</Link>
+			</p>
+
+
+			<p className="text-center results-font" style={{color: "#56A8CBFF", fontSize: "1.9em"}}>
+				~ 3rd Place Semi-Finalists ~
+			</p>
+			<p>
+				<Link to={`/player/${thirdPlaceWinners[0]._id}`} className="remove-underline">
+					<Media className="media-element media-hover">
+						<Media left>
+							<DirectoryImage key={thirdPlaceWinners[0]._id} main={thirdPlaceWinners[0].main} />
+						</Media>
+						<Media body>
+							<Media heading className="ml-3">
+								<b>{ thirdPlaceWinners[0].username }</b>
+							</Media>
+							<i style={{fontSize:"0.9rem"}}>
+								{
+									thirdPlaceWinners[0].main && thirdPlaceWinners[0].secondary ?
+									<Media className="ml-3">{ thirdPlaceWinners[0].main + ", " + thirdPlaceWinners[0].secondary }</Media> :
+									<Media className="ml-3">{ thirdPlaceWinners[0].main || thirdPlaceWinners[0].secondary || null }</Media>
+								}
+								<Media className="ml-3">{ thirdPlaceWinners[0].friendCode }</Media>
+							</i>
+						</Media>
+					</Media>
+				</Link>
+			</p>
+
+			<p>
+				<Link to={`/player/${thirdPlaceWinners[1]._id}`} className="remove-underline">
+					<Media className="media-element media-hover">
+						<Media left>
+							<DirectoryImage key={thirdPlaceWinners[1]._id} main={thirdPlaceWinners[1].main} />
+						</Media>
+						<Media body>
+							<Media heading className="ml-3">
+								<b>{ thirdPlaceWinners[1].username }</b>
+							</Media>
+							<i style={{fontSize:"0.9rem"}}>
+								{
+									thirdPlaceWinners[1].main && thirdPlaceWinners[1].secondary ?
+									<Media className="ml-3">{ thirdPlaceWinners[1].main + ", " + thirdPlaceWinners[1].secondary }</Media> :
+									<Media className="ml-3">{ thirdPlaceWinners[1].main || thirdPlaceWinners[1].secondary || null }</Media>
+								}
+								<Media className="ml-3">{ thirdPlaceWinners[1].friendCode }</Media>
+							</i>
+						</Media>
+					</Media>
+				</Link>
+			</p>
+
+			<div className="text-center" style={{color:"lightgrey", backgroundColor: "#333333"}}>
+				<h1>{ title }</h1>
+				<h4>By { hostedBy }</h4>
+				<h4>{participants && participants.length}-player bracket</h4>
+				<BracketGenerator
+					bracketSize={participants && participants.length}
+					rounds={bracket.rounds}
+					players={bracket.players}
+					scores={bracket.scores}
+				/>
+			</div>
 			<br /><Link to="/">Back to Tournaments main page</Link>
     </div>
   );
